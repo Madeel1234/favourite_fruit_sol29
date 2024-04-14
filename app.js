1a07912978196ddd2c7b2b40d529d47eb43199c7
Madeel1234 <164627451+Madeel1234@users.noreply.github.com>
2024-04-14 17:44:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+// Task # 29
+let favourite_fruits = ["KIVI", "ORANGE", "APPLES", "PEACH", "BERRY"];
+if (favourite_fruits.includes("KIVI")) {
+    console.log("KIVI");
+}
+if (favourite_fruits.includes("PEACH")) {
+    console.log("you really like bananas");
+}
+if (favourite_fruits.includes("ORANGE")) {
+    console.log("ORANGE");
+}
+if (favourite_fruits.includes("BERRY")) {
+    console.log("you really like bananas");
+}
+if (favourite_fruits.includes("APPLES")) {
+    console.log("APPLES");
+}
+export {};
---
+++ app.ts
@@ -0,0 +1,19 @@
+// Task # 29
+
+let favourite_fruits: string [] = ["KIVI","ORANGE","APPLES","PEACH","BERRY"]
+
+if (favourite_fruits.includes("KIVI")) {
+    console.log("KIVI")
+}
+if (favourite_fruits.includes("PEACH")) {
+    console.log("you really like bananas")
+}
+if (favourite_fruits.includes("ORANGE")) {
+    console.log("ORANGE")
+}
+if (favourite_fruits.includes("BERRY")) {
+    console.log("you really like bananas")
+}
+if (favourite_fruits.includes("APPLES")) {
+    console.log("APPLES")
+}
